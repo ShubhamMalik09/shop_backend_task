6894e3d7c4cf9af54268e461149daf21d26faa82
Shubham <shubhammalik.sm05@gmail.com>
2024-10-15 22:48:28 +0530

    created model

--- Shops/models.py
+++ Shops/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)]
+    )
+    longitude = models.DecimalField(
+        max_digits=9, 
+        decimal_places=6,
+        validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)]
+    )
+
+    def __str__(self):
+        return self.name
